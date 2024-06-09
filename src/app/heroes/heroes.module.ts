9f00140a2e7eca4b1159e7e3cee0016e24fe4575
kathm <kathmontalvo123@gmail.com>
2024-06-09 00:47:52 -0500

    add heroes db

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -8,6 +8,8 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
+import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 
 @NgModule({
@@ -16,7 +18,9 @@ import { MaterialModule } from '../material/material.module';
     LayoutPageComponent,
     ListPageComponent,
     NewHeroPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    CardComponent,
+    HeroImagePipe
   ],
   imports: [
     CommonModule,
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of, pipe } from 'rxjs';
+import { environments } from '../../../environments/environments';
+import { Hero } from '../interfaces/hero.interface';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+  constructor(private http: HttpClient) { }
+
+  private baseUrl: string = environments.baseUrl;
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError( error => of(undefined))
+    )
+  }
+}
